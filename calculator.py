074d82ff0d34015a3ba1bf80151b9ce5d010c706
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-24 16:06:55 -0700

    added - subtract function - multiply function - divide function - square function

--- calculator.py
+++ calculator.py
@@ -19,5 +19,17 @@ def calculator():
         elif tokenized_input[0] == '+':
             add_function = add(int(tokenized_input[1]), int(tokenized_input[2]))
             print add_function
+        elif tokenized_input[0] == '-':
+            subtract_function = subtract(int(tokenized_input[1]), int(tokenized_input[2]))
+            print subtract_function
+        elif tokenized_input[0] == '*':
+            multiply_function = multiply(int(tokenized_input[1]), int(tokenized_input[2]))
+            print multiply_function
+        elif tokenized_input[0] == '/':
+            divide_function = divide(int(tokenized_input[1]), int(tokenized_input[2]))
+            print divide_function
+        elif tokenized_input[0] == 'square':
+            square_function = square(int(tokenized_input[1]))
+            print square_function
 
 calculator()
