4cc1d819d76faf9f8ef3390120ea568e17855bb7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-24 18:07:31 -0700

    WIP - Working on casting token into int once on tokenized_input - Working on printing once at the end of while loop - Also look into further study

--- calculator.py
+++ calculator.py
@@ -10,35 +10,53 @@ from arithmetic import *
 # Your code goes here
 def calculator():
     """Calculates basic math operations using prefix notation"""
+
     while True:
         user_input = raw_input('> ')
-        tokenized_input = user_input.split(' ')
+        tokenized_input = user_input.split()
+
+            # for item in tokenized_input[1:]:
+            #     int(item) and append it to original tokenized_input
+
+        sliced_ti = int(tokenized_input[1:])
+            # make each item on the list its own thing?? Assign each
+            #to a different variable??
+        tokenized_input[1:].append(sliced_ti)
+
         if tokenized_input[0] == 'q':
             print 'Goodbye!'
             break
+
         elif tokenized_input[0] == '+':
-            add_function = add(int(tokenized_input[1]), int(tokenized_input[2]))
+            add_function = add(tokenized_input[1], tokenized_input[2])
             print add_function
+
         elif tokenized_input[0] == '-':
-            subtract_function = subtract(int(tokenized_input[1]), int(tokenized_input[2]))
+            subtract_function = subtract(tokenized_input[1], tokenized_input[2])
             print subtract_function
+
         elif tokenized_input[0] == '*':
-            multiply_function = multiply(int(tokenized_input[1]), int(tokenized_input[2]))
+            multiply_function = multiply(tokenized_input[1], tokenized_input[2])
             print multiply_function
+
         elif tokenized_input[0] == '/':
-            divide_function = divide(int(tokenized_input[1]), int(tokenized_input[2]))
+            divide_function = divide(tokenized_input[1], tokenized_input[2])
             print divide_function
+
         elif tokenized_input[0] == 'square':
-            square_function = square(int(tokenized_input[1]))
+            square_function = square(tokenized_input[1])
             print square_function
+
         elif tokenized_input[0] == 'cube':
-            cube_function = cube(int(tokenized_input[1]))
+            cube_function = cube(tokenized_input[1])
             print cube_function
+
         elif tokenized_input[0] == 'pow':
-            power_function = power(int(tokenized_input[1]), int(tokenized_input[2]))
+            power_function = power(tokenized_input[1], tokenized_input[2])
             print power_function
+
         elif tokenized_input[0] == 'mod':
-            mod_function = mod(int(tokenized_input[1]), int(tokenized_input[2]))
+            mod_function = mod(tokenized_input[1], tokenized_input[2])
             print mod_function
 
 calculator()
